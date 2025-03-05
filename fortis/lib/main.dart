c380165dba7b1f1c718d334586ab0dcd0103fc0a
Cameron Sevin <csevin3@lsu.edu>
2025-03-05 16:53:27 -0600

    Added bottom navigation bar and pages

---
+++ fortis/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'pages/home.dart';
+import 'pages/relax.dart';
+import 'pages/profile.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _pages = [
+    const HomePage(),
+    const RelaxPage(),
+    const ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex], // Display the selected page
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.tag_faces), label: 'Relax'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile')
+        ],
+      ),
+    );
+  }
+}
---
+++ fortis/lib/pages/relax.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class RelaxPage extends StatelessWidget {
+  const RelaxPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Take a Moment to Relax'), centerTitle: true),
+      body: const Center(
+        child: Text(
+          'Relax Page Content',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
