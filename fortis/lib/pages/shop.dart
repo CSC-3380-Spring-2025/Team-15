75fa97e670e190d2364f6345e82c1ec2c461e041
MarkPlaisance <mplaisance26@gmail.com>
2025-03-22 19:37:35 -0500

    Update shop.dart

--- fortis/lib/pages/shop.dart
+++ fortis/lib/pages/shop.dart
@@ -5,18 +5,19 @@ class ShopPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<String?> items  = List.generate(15, (index) => null); // Placeholder items
+    List<String?> items  = List.generate(15, (index) => null); // Placeholder for items
 
     return Scaffold(
       appBar: AppBar(title : const Text('Welcome to the Shop'), centerTitle: true),
       body: Padding(padding: const EdgeInsets.all(8.0),
       child: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount ( // Builds the grid
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount (
           crossAxisCount: 3, // 3 columns
-          crossAxisSpacing: 8, // Space between columns
+          crossAxisSpacing: 8, // space between columns
           mainAxisSpacing: 8, // space between rows
           childAspectRatio: 0.8, // width to height ratio
           ),
+
           itemCount: items.length,
           itemBuilder: (context, index) {
             return Card(
