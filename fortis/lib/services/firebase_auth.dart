5a34054399b8c0058a73411c794cd2a472a7d893
Brandon Tran <132234585+brandont120@users.noreply.github.com>
2025-03-29 17:26:57 -0500

    Added user login authentication with firebase

---
+++ fortis/lib/services/firebase_auth.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class FirebaseAuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<User?> signUpWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential credential = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The email is already in use.');
+      }
+      return null;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  Future<User?> signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential credential = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user was found under that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password.');
+      }
+      return null;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+}
