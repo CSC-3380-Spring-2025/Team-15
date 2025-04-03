d4db9aba15fb84d7cbd87ae6de420ec96dfd107a
keezyttg <125530188+keezyttg@users.noreply.github.com>
2025-04-03 13:53:27 -0500

    Uploading audio, and user_login
    Fixing errors with user_login and uploading audio for breathing page

--- fortis/lib/pages/user_login.dart
+++ fortis/lib/pages/user_login.dart
@@ -11,67 +11,33 @@ class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageState();
 }
 
-<<<<<<< Updated upstream
-  class _LoginPageState extends State<LoginPage> {
-    TextEditingController emailController = TextEditingController();
-    TextEditingController passwordController = TextEditingController();
-    final FirebaseAuthService _firebaseAuthService = FirebaseAuthService();
-
-    void _signIn() async {
-      String email = emailController.text;
-      String password = passwordController.text;
-      User? user = await _firebaseAuthService.signInWithEmailAndPassword(email, password);
-
-      if (user != null) {
-        print("Login successful");
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => const MainScreen()),
-        );
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text("Login failed, email or password is incorrect")),
-        );
-      }
-    }
-
-    @override
-    Widget build(BuildContext context){
-      return Scaffold(
-        appBar: AppBar(title: const Text('Login')),
-        body: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: ListView(
-            children: <Widget>[
-              Container(
-                alignment: Alignment.center, 
-                padding: const EdgeInsets.all(10), 
-                child: const Text(
-                  'Fortis', 
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 30)
-                    )),
-              Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Sign in',
-                  style: TextStyle(fontSize: 20),
-                )),
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: TextField(
-                  controller: emailController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Email',
-                  ),
-=======
 class _LoginPageState extends State<LoginPage> {
-  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  final FirebaseAuthService _firebaseAuthService = FirebaseAuthService();
+
+  void _signIn() async {
+    String email = emailController.text;
+    String password = passwordController.text;
+    User? user = await _firebaseAuthService.signInWithEmailAndPassword(
+      email,
+      password,
+    );
+
+    if (user != null) {
+      print("Login successful");
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const MainScreen()),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Login failed, email or password is incorrect"),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -90,7 +56,6 @@ class _LoginPageState extends State<LoginPage> {
                   color: Colors.black,
                   fontWeight: FontWeight.w500,
                   fontSize: 30,
->>>>>>> Stashed changes
                 ),
               ),
             ),
@@ -102,21 +67,12 @@ class _LoginPageState extends State<LoginPage> {
             Container(
               padding: const EdgeInsets.all(10),
               child: TextField(
-                controller: nameController,
+                controller: emailController,
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
-                  labelText: 'User Name',
+                  labelText: 'Email',
                 ),
               ),
-<<<<<<< Updated upstream
-              Container(
-                height: 50,
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: ElevatedButton(
-                  child: const Text('Login'),
-                  onPressed: _signIn,
-                )
-=======
             ),
             Container(
               padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
@@ -127,31 +83,14 @@ class _LoginPageState extends State<LoginPage> {
                   border: OutlineInputBorder(),
                   labelText: 'Password',
                 ),
->>>>>>> Stashed changes
               ),
             ),
             Container(
               height: 50,
-              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+              margin: const EdgeInsets.fromLTRB(10, 20, 10, 0),
               child: ElevatedButton(
+                onPressed: _signIn,
                 child: const Text('Login'),
-                onPressed: () {
-                  if (nameController.text.isNotEmpty &&
-                      passwordController.text.isNotEmpty) {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const MainScreen(),
-                      ),
-                    );
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('Please enter username and password'),
-                      ),
-                    );
-                  }
-                },
               ),
             ),
           ],
